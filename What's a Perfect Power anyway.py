a3311a1c3ec7bec9edfa5cc4b98fe7f1e270672c
lidefo <lidefo753@gmail.com>
2022-04-28 15:19:14 +0500

    What's a Perfect Power anyway?

---
+++ What's a Perfect Power anyway.py
@@ -0,0 +1,42 @@
+'''
+5 kyu
+
+A perfect power is a classification of positive integers:
+
+In mathematics, a perfect power is a positive integer that can be expressed as an integer power of another positive
+integer. More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that mk = n.
+
+Your task is to check wheter a given integer is a perfect power. If it is a perfect power, return a pair m and k with
+m**k = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+
+Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2, so (3,4) and (9,2) are valid
+solutions. However, the tests take care of this, so if a number is a perfect power, return any pair that proves it.
+
+Examples
+isPP(4) => [2,2]
+isPP(9) => [3,2]
+isPP(5) => None
+
+https://www.codewars.com/kata/54d4c8b08776e4ad92000835
+'''
+
+
+def isPP(n):
+    m = 2
+    while m <= n // 2:
+        k = 2
+
+        while m ** k <= n:
+            if m ** k == n:
+                return [m, k]
+            k += 1
+
+        m += 1
+
+    return None
+
+
+
+
+
+print(isPP(5))
