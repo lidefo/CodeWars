3ce9ad452117cded93d46fba83a0aa08dfe9b61f
lidefo <lidefo753@gmail.com>
2022-05-19 17:52:23 +0500

    7 kyu katas

---
+++ 7 kyu/Square Every Digit.py
@@ -0,0 +1,15 @@
+'''
+7 kyu
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+
+https://www.codewars.com/kata/546e2562b03326a88e000020
+'''
+
+
+def square_digits(num):
+    return int(''.join([str(int(digit) ** 2) for digit in str(num)]))
